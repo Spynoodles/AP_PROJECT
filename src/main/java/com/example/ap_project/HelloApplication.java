450a91244e8724c811e34b01eaac8238ae55fb75
Spynoodles <khushaly10@gmail.com>
2023-11-15 20:12:10 +0530

    Kardiya bhai push

--- src/main/java/com/example/ap_project/HelloApplication.java
+++ src/main/java/com/example/ap_project/HelloApplication.java
@@ -8,12 +8,17 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
+    private String Character;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene Main_menu = new Scene(fxmlLoader.load(), 800, 600);
+//        Scene live=new Scene(fxmlLoader.load(),320,240);
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+        stage.setScene(Main_menu);
         stage.show();
     }
 
